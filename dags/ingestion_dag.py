8748c5168677c0fe961c402fec00a5c2cd2f3352
gh4rris <gharris90@hotmail.co.uk>
2026-03-31 12:02:54 +0100

    update dag layout

--- dags/ingestion_dag.py
+++ dags/ingestion_dag.py
@@ -1,27 +1,31 @@
-from airflow.sdk import DAG, task
+from airflow.sdk import dag, task
 from datetime import datetime
 
 
-@task
-def scrape_articles_and_load_to_database() -> None:
-    from ingestion import scrape_articles_and_load_to_database
-    scrape_articles_and_load_to_database()
+@dag(
+        dag_id="news_ingestion_and_process",
+        start_date=datetime(2026, 3, 5),
+        schedule="@daily",
+        catchup=False
+)
+def news_ingestion_and_process() -> None:
+
+    @task.python
+    def scrape_articles_and_load_to_database() -> None:
+        from ingestion import scrape_articles_and_load_to_database
+        scrape_articles_and_load_to_database()
 
 
-@task
-def process_articles_and_load_to_database() -> None:
-    from nlp_processing import process_articles_and_load_to_database
-    process_articles_and_load_to_database()
+    @task.python
+    def process_articles_and_load_to_database() -> None:
+        from nlp_processing import process_articles_and_load_to_database
+        process_articles_and_load_to_database()
 
-
-with DAG(
-    dag_id="news_ingestion_and_process",
-    start_date=datetime(2026, 3, 5),
-    schedule="@daily",
-    catchup=False
-) as dag:
     
     scrape = scrape_articles_and_load_to_database()
     process = process_articles_and_load_to_database()
 
     scrape >> process
+
+
+news_ingestion_and_process()
